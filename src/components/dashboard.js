952ca54101822abedc9915abd4c34d0907307c8e
Kyle Middleton <kmiddleton654@gmail.com>
2019-02-02 21:09:07 -0700

    added final score screen at end with conditional

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -10,47 +10,92 @@ export default class Dashboard extends React.Component {
             correct: 0,
             incorrect: 0,
             questionNumber: 0,
-            answer: ''
-           
+            answer: '',
+            showQuestion: true
         }
+    }
+
+    showQuestion() {
+      while (this.state.incorrect + this.state.correct !== QUESTIONS_LIST.length) {
+        let currentQuestion = QUESTIONS_LIST[this.state.questionNumber].question
+          let currentAnswerChoices = []
+          for (let i=0; i<QUESTIONS_LIST[this.state.questionNumber].answers.length; i++) {
+            currentAnswerChoices.push(QUESTIONS_LIST[this.state.questionNumber].answers[i])
+          }
+          
+          return (
+            
+            <div>
+              <Question question={currentQuestion} />
+              {currentAnswerChoices.map((answer, index) => {
+                return <button key={index} value={answer} onClick={() => {
+                  this.setState({answer: answer, showQuestion: false}, () => this.onAnswer())
+                  }}>{answer}</button>
+              })}
+            
+              
+            </div>
+          )
+      }
+            
     }
 
     onAnswer() {
       let correctAnswer = QUESTIONS_LIST[this.state.questionNumber].correctAnswer
-      console.log('this.state.answer ' + this.state.answer, 'correctanswer' + correctAnswer)
       if (this.state.answer === correctAnswer) {
-        this.setState({correct: this.state.correct+1, questionNumber: this.state.questionNumber+1})
+        this.setState({correct: this.state.correct+1, questionNumber: this.state.questionNumber+1}, () => this.rightAnswer()
+        )
       }
       else {
-        this.setState({incorrect: this.state.incorrect+1, questionNumber: this.state.questionNumber+1})
+        this.setState({incorrect: this.state.incorrect+1, questionNumber: this.state.questionNumber+1}, () => this.wrongAnswer()
+      
+        );
       }
+     
+    }
 
+ 
+    
+
+    rightAnswer() {
+      if (this.state.showQuestion === false) {
+        return (
+          <div>
+            <p>Correct!</p>
+            <button onClick={() => this.setState({showQuestion: true})}></button>
+          </div>
+
+        )
+      }
+    }
+
+    wrongAnswer() {
+      if (this.state.showQuestion === false) {
+        return (
+          <div>
+            <p>Wrong.</p>
+            <button onClick={() => this.setState({showQuestion: true})}></button>
+          </div>
+        )
+      }
+    }
+
+    finalScore() {
+      return (
+        <div>
+            <p>Correct: {this.state.correct}</p>
+            <p>Incorrect: {this.state.incorrect}</p>
+        </div>
+      )
     }
     
     render() {
-        let currentQuestion = QUESTIONS_LIST[this.state.questionNumber].question
-        let currentAnswerChoices = []
-        for (let i=0; i<QUESTIONS_LIST[this.state.questionNumber].answers.length; i++) {
-          currentAnswerChoices.push(QUESTIONS_LIST[this.state.questionNumber].answers[i])
-        }
-        
-        return (
-          
-          <div>
-            <Question question={currentQuestion} />
-            {currentAnswerChoices.map((answer, index) => {
-              return <button key={index} value={answer} onClick={() => {
-                this.setState({answer: answer}, () => this.onAnswer())
-                }}>{answer}</button>
-            })}
-            <p>Correct: {this.state.correct}</p>
-            <p>Incorrect: {this.state.incorrect}</p>
-            
-          </div>
-        )
+      if (this.state.correct + this.state.incorrect === QUESTIONS_LIST.length) {
+        return this.finalScore()
+      }
+      else {
+        return this.showQuestion()
+      }
       
-        
-      
-        
     }
 }
