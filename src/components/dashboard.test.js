3558b2b825c89ddcf633ad2848c552ea567b4f3c
Kyle Middleton <kmiddleton654@gmail.com>
2019-02-05 15:14:10 -0700

    added tests, including seeing if component displays four answer buttons

--- src/components/dashboard.test.js
+++ src/components/dashboard.test.js
@@ -4,18 +4,16 @@ import {shallow, mount} from 'enzyme'
 import Dashboard from './dashboard';
 
 it('renders without crashing', () => {
-  //const div = document.createElement('div');
-  //ReactDOM.render(<Dashboard />, div);
-  //ReactDOM.unmountComponentAtNode(div);
   shallow(<Dashboard />)
 });
 
 it('should display a question', () => {
     const wrapper = shallow(<Dashboard />)
-    expect(wrapper.hasClass('question-wrapper'))
+    expect(wrapper.hasClass('question-wrapper')).toEqual(true)
 })
 
-it('should display buttons with answers', () => {
+it('should display four answer buttons', () => {
     const wrapper = shallow(<Dashboard />)
-    expect(wrapper.hasClass('dashboard-answer-button'))
+    const findButtons = wrapper.find('.dashboard-answer-button')
+    expect(findButtons).toHaveLength(4)
 })
