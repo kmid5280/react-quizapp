d4cb79b2bba2c0e8e28754f9210f20c5b83cf776
Kyle Middleton <kmiddleton654@gmail.com>
2019-02-04 19:44:29 -0700

    bug fix

--- src/components/title.js
+++ src/components/title.js
@@ -3,12 +3,8 @@ import React from 'react'
 export default function Title(props) {
     return (
         <div>
-            <header>
-                <h1 className="header-title">State Capital Quiz</h1>
-            </header>
-            <body>
-                <button onClick={props.startGame.bind(this)}>Begin</button>
-            </body>
+           <h1 className="header-title">State Capital Quiz</h1>
+           <button onClick={props.startGame.bind(this)}>Begin</button>
         </div>
     )
 }
