c90dc9aba770ce5c876e213d78793c576ed2fec7
Kyle Middleton <kmiddleton654@gmail.com>
2019-02-05 17:05:40 -0700

    added test to ensure answer is shown when any answer button is clicked

--- src/components/dashboard.test.js
+++ src/components/dashboard.test.js
@@ -17,3 +17,13 @@ it('should display four answer buttons', () => {
     const findButtons = wrapper.find('.dashboard-answer-button')
     expect(findButtons).toHaveLength(4)
 })
+
+it('should display answer screen when answer clicked', () => {
+    function generateRandom(min, max) {
+        return Math.floor(Math.random() * (max-min) + min)
+    }
+    const wrapper = mount(<Dashboard/>)
+    const findButton = wrapper.find('.dashboard-answer-button')
+    findButton.at(generateRandom(0,4)).simulate('click')
+    expect(wrapper.find('.show-answer')).toHaveLength(1)
+})
