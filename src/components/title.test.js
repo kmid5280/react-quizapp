aea243eaf9df87be6b9ca4e3d7835b7f9d680ce4
Kyle Middleton <kmiddleton654@gmail.com>
2019-02-10 16:12:19 -0700

    added test to check if title rendered

--- src/components/title.test.js
+++ src/components/title.test.js
@@ -8,6 +8,11 @@ it('renders without crashing', () => {
     shallow(<Title/>)
 })
 
+it('renders a title', () => {
+    const wrapper = shallow(<Title/>)
+    expect(wrapper.find('.header-title')).toHaveLength(1)
+})
+
 it('renders a start button', () => {
     const wrapper = shallow(<Title/>)
     expect(wrapper.find('.start-button')).toHaveLength(1)
