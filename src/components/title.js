fa0433667cc70d3e527d2fa1865a15a4b3f7b39e
Kyle Middleton <kmiddleton654@gmail.com>
2019-02-10 21:22:18 -0700

    added padding, margins, and media queries

--- src/components/title.js
+++ src/components/title.js
@@ -8,6 +8,7 @@ export default function Title(props) {
                 <h1 className="header-title">State Capital Quiz</h1>
             </header>
             <main role="main" aria-live="polite">
+                <p className="instructions">Test your knowledge of U.S. State Capitals with this quiz! Click or tap the "Begin" button to start.</p>
                 <button className="start-button" onClick={props.startGame}>Begin</button>
             </main>
         </div>
