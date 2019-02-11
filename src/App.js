337d67b7a79e1dd2309df2cd797a332599c00746
Kyle Middleton <kmiddleton654@gmail.com>
2019-02-10 22:30:19 -0700

    added react-meta-tags package and meta tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Helmet} from 'react-helmet'
+import MetaTags from 'react-meta-tags'
 import Title from './components/title';
 import Dashboard from './components/dashboard'
 import './App.css';
@@ -19,9 +20,14 @@ class App extends Component {
   }
   
   render() {
+    
     if (this.state.gamePlay === false) {
       return (
         <div>
+          <MetaTags>
+            <meta id="meta-description" name="description" content="A U.S. State Capital Quiz using React." />
+            <meta id="meta-keywords" name="keywords" content="Quiz, React, State Capital, United States, USA" />
+          </MetaTags>
           <Helmet>
             <html lang={this.state.lang} />
           </Helmet>
@@ -32,6 +38,11 @@ class App extends Component {
     else {
       return (
       <div>
+        <MetaTags>
+          <meta name="title" content="U.S. State Capital Quiz" />
+          <meta name="description" content="A U.S. State Capital Quiz using React." />
+          <meta name="keywords" content="Quiz, React, State Capital, United States, USA" />
+        </MetaTags>
         <Helmet>
           <html lang={this.state.lang} />
         </Helmet>
