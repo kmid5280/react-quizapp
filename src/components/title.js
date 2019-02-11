517edfbc722bcb99af8e3cf91bdce5f47b3429de
Kyle Middleton <kmiddleton654@gmail.com>
2019-02-10 22:28:28 -0700

    changed title to U.S. State Capital Quiz

--- src/components/title.js
+++ src/components/title.js
@@ -5,7 +5,7 @@ export default function Title(props) {
     return (
         <div>
             <header role="banner" aria-live="polite">
-                <h1 className="header-title">State Capital Quiz</h1>
+                <h1 className="header-title">U.S. State Capital Quiz</h1>
             </header>
             <main role="main" aria-live="polite">
                 <p className="instructions">Test your knowledge of U.S. State Capitals with this quiz! Click or tap the "Begin" button to start.</p>
