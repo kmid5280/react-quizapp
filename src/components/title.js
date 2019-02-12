4b004a2a0aa2143b813d59386456c9957384e702
Kyle Middleton <kmiddleton654@gmail.com>
2019-02-11 19:59:11 -0700

    changed width of instructions

--- src/components/title.js
+++ src/components/title.js
@@ -8,8 +8,10 @@ export default function Title(props) {
                 <h1 className="header-title">U.S. State Capital Quiz</h1>
             </header>
             <main role="main" aria-live="polite">
+            <div className="instructions-wrapper">
                 <p className="instructions">Test your knowledge of U.S. State Capitals with this quiz! Click or tap the "Begin" button to start.</p>
-                <button className="start-button" onClick={props.startGame}>Begin</button>
+            </div>
+            <button className="start-button" onClick={props.startGame}>Begin</button>
             </main>
         </div>
     )
