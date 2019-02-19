0330432548103f6dc03dac293fd1d729d8b378ea
Kyle Middleton <kmiddleton654@gmail.com>
2019-02-18 22:02:22 -0700

    initial commit

---
+++ src/components/actions.js
@@ -0,0 +1,9 @@
+export const SHOW_QUESTION = 'SHOW_QUESTION'
+export const showQuestion = () => ({
+    type: SHOW_QUESTION
+})
+
+export const SHOW_ANSWER = 'SHOW_ANSWER'
+export const showAnswer = () => ({
+    type: SHOW_ANSWER
+})
---
+++ src/components/reducers.js
@@ -0,0 +1,12 @@
+import {SHOW_QUESTION} from './actions'
+
+const initialState = {
+    correct: 0,
+    incorrect: 0,
+    questionNumber: 0,
+    answeredQuestions: 0,
+    answer: '',
+    showQuestion: true,
+    answerCorrect: false,
+    correctAnswer: ''
+}
